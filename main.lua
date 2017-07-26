d751137c17b33e7ecb695d6a9f5441221086768f
Mark Durrant <mark.j.durrant@gmail.com>
2017-07-26 23:46:10 +0100

    draw some svg <line> elements

--- main.lua
+++ main.lua
@@ -13,28 +13,41 @@ local svgHead = '<?xml version="1.0" encoding="utf-8"?>\n' ..
                 '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" ' ..
                 'xmlns:xlink="http://www.w3.org/1999/xlink" ' ..
                 'width="' .. page.x .. 'mm" height="' .. page.y .. 'mm" ' ..
-                'viewbox="0 0 ' .. page.x .. ' ' .. page.y .. '">\n'
+                'viewbox="0 0 ' .. page.x .. ' ' .. page.y .. '">'
 
 -- add style tag for all elements
-local svgStyle =
-[[
-  <style>
-    * {
-      fill: none;
-      stroke-width: ]] .. pen.thickness .. [[;
-      stroke: ]] .. pen.color .. [[;
-    }
-  </style>
-]]
-
---  svg content
-local svgBody = '\n  <rect x="10" y="10" width="100" height="100"/>'
+local svgStyle = '<style>* {fill: none; stroke-width: ' .. pen.thickness ..
+                 '; stroke: ' .. pen.color .. ';}</style>'
 
 -- add svg closing tag
-local svgClose = '\n</svg>'
+local svgClose = '</svg>'
+
+-- create a new point
+local point = function (x, y)
+  local point = {}
+        point.x = x
+        point.y = y
+        point.log = function ()
+          print("point: " .. point.x .. ", " .. point.y)
+        end
+  return point
+end
+
+-- draw a line
+local line = function (from, to)
+  return '<line x1="' .. from.x .. '" y1="' .. from.y .. '" ' ..
+         'x2="' .. to.x .. '" y2="' .. to.y .. '"/>'
+end
+
+--  svg body
+local svgBody = line(point(0, 0), point(page.x, page.y)) ..
+                line(point(page.x, 0), point(0, page.y))
 
 -- concat all svg content to write to file
-local svgContent = svgHead .. svgStyle .. svgBody .. svgClose
+local svgContent = svgHead  ..
+                   svgStyle ..
+                   svgBody  ..
+                   svgClose
 
 -- file config
 local directory = './svg-output/'
