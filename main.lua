b45bfa38cd25505299f449e9d23ebcd2fb27bd87
Mark Durrant <mark.j.durrant@gmail.com>
2017-08-02 21:03:56 +0100

    split out line

--- main.lua
+++ main.lua
@@ -2,16 +2,11 @@
 local   page = require 'modules/page'
 local    pen = require 'modules/pen'
 local  point = require 'modules/point'
+local   line = require 'modules/line'
 local svgGen = require 'modules/svg-gen'
-
--- draw a line
-local line = function (from, to)
-  return '<line x1="' .. from.x .. '" y1="' .. from.y .. '" ' ..
-         'x2="' .. to.x .. '" y2="' .. to.y .. '"/>'
-end
 
 --  svg body
 local svgBody = line(point(0, 0), point(page.x, page.y)) ..
-                line(point(page.x, 0), point(0, page.y))
+                line(point(0, page.y), point(page.x, 0))
 
-svgGen('svg-output/test2.svg', page, pen, svgBody)
+svgGen('svg-output/test.svg', page, pen, svgBody)
--- modules/point.lua
+++ modules/point.lua
@@ -3,9 +3,6 @@ local point = function (x, y)
   local point = {}
         point.x = x
         point.y = y
-        point.log = function ()
-          print("point: " .. point.x .. ", " .. point.y)
-        end
   return point
 end
 
