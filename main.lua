ab7352f72ca37f6828ab200398bb487278d1ad59
Mark Durrant <mark.j.durrant@gmail.com>
2017-08-21 22:51:26 +0100

    add temp table logging

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local log = require("modules/logTable")
+
 local paper = require("modules/paper")
 local pen = require("modules/pen")
 local path = require("modules/path")
@@ -7,3 +9,5 @@ local myPaper = paper:new(297, 210)
 local myPen = pen:new(5, "#f09")
 local myPath = path:new()
 local myPoint = point:new(50, 100)
+
+log(myPen)
