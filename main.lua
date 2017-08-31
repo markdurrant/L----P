aee01e8ef6ee24e1325affc297166f2604d6c0f8
Mark Durrant <mark.j.durrant@gmail.com>
2017-08-31 22:20:29 +0100

    refactor & comment Point.lua

--- main.lua
+++ main.lua
@@ -11,12 +11,5 @@ local paper = Paper:new({ width = 500, height = 500 })
 local black = Pen:new({ weight = 2, color = '#000' })
       paper:addPen(black)
 
-p1 = Point:new({ x = 100, y = 100 })
-p2 = Point:new({ x = 200, y = 200 })
-
-l1 = Shape.Line(p1, p2)
-l1:setPen(black)
-
-paper:log()
-
 paper:saveTo('svg-output/testy.svg')
+paper:preview('html-preview/preview.html')
--- modules/point.lua
+++ modules/point.lua
@@ -1,26 +1,32 @@
+-- get utilities 
 local utl = require("modules/utl")
 
-local Point = { label = "point" }
+-- set up Point table & label
+local Point = { label = "Point" }
       Point.metatable = { __index = Point }
 
-function Point:new(t)
-  if not t then t = {} end
-  if not t.x then t.x = 0 end
-  if not t.y then t.y = 0 end
+-- create a new point with X & Y
+function Point:new(x, y)
+  local point = {}
+        point.x = x or 0
+        point.y = y or 0
 
-  setmetatable(t, Point.metatable)
+  setmetatable(point, Point.metatable)
 
-  return t
+  return point
 end
 
-function Point:log()
-  print(string.format("point { x = %s, y = %s }", self.x, self.y))
+-- create a copy of the point
+function Point:clone()
+  return utl.clone(self)
 end
 
+-- add the point to a path
 function Point:setPath(path)
   table.insert(path, self)
 end
 
+-- move the point in X & Y
 function Point:move(x, y)
   if not y then y = x end
 
@@ -28,23 +34,40 @@ function Point:move(x, y)
   self.y = self.y + y
 end
 
-function Point:rotate(angle, origin)
+-- move the point in X
+function Point:moveX(x)
+  self.x = self.x + x
+end
+
+-- move the point in Y
+function Point:moveY(y)
+  self.y = self.y + y
+end
+
+-- move the point along a vector (angle & distance)
+-- North = 0, East = 90, South = 180, West = 270
+function Point:moveVector(angle, distance)
+  angle = math.rad(angle - 90)
+
+  self.x = self.x + math.cos(angle) * distance
+  self.y = self.y + math.sin(angle) * distance
+end
+
+-- rotate the point around an origin (point) in degrees
+function Point:rotate(point, angle)
   local radians = math.rad(angle)
 
-  local x1 = self.x - origin.x
-  local y1 = self.y - origin.y
+  local x1 = self.x - point.x
+  local y1 = self.y - point.y
 
   local x2 = x1 * math.cos(radians) - y1 * math.sin(radians)
   local y2 = x1 * math.sin(radians) + y1 * math.cos(radians)
 
-  self.x = x2 + origin.x
-  self.y = y2 + origin.y
+  self.x = x2 + point.x
+  self.y = y2 + point.y
 end
 
-function Point:clone()
-  return utl.clone(self)
-end
-
+-- get the distance to a second point
 function Point:getDistanceTo(point)
   local a = self.x - point.x
   local b = self.y - point.y
@@ -64,6 +87,8 @@ function Point:getDistanceTo(point)
   return distance
 end
 
+-- get the angle to a second point in degrees
+-- North = 0, East = 90, South = 180, West = 270
 function Point:getAngleTo(point)
   local a = self.x - point.x
   local b = self.y - point.y
@@ -79,4 +104,10 @@ function Point:getAngleTo(point)
   return angle
 end
 
+-- print point details
+function Point:log()
+  print(string.format("point { x = %s, y = %s }", self.x, self.y))
+end
+
+-- return the Point module 
 return Point
