60f2c6274e495653a7ca8c069af70fa4f5724fcb
Mark Durrant <mark.j.durrant@gmail.com>
2017-09-04 23:51:37 +0100

    a drawing

--- main.lua
+++ main.lua
@@ -8,37 +8,35 @@ local Point = require("modules/point")
 local Shape = require("modules/shape")
 
 local paper = Paper:new({ width = 500, height = 500 })
-local black = Pen:new({ weight = 2, color = "#000" })
-local red = Pen:new({ weight = 2, color = "#f00" })
-      paper:addPens(black, red)
+local pink = Pen:new({ weight = 1.4, color = "#f09" })
+local blue = Pen:new({ weight = 1.4, color = "#09f" })
+      paper:addPens(blue, pink)
 
-local size = 100
+local baseAngle = 210
 
-local square = Path:new(
-  Point:new(paper.center.x + size / 2, paper.center.y - size / 2),
-  Point:new(paper.center.x + size / 2, paper.center.y + size / 2),
-  Point:new(paper.center.x - size / 2, paper.center.y + size / 2),
-  Point:new(paper.center.x - size / 2, paper.center.y - size / 2)
-)
-square:setPen(black)
+local p1 = Point:new(220, 140)
+local p2 = Point:new(420, 260)
+local p3 = Point:new(300, 250)
+local p4 = Point:new(120, 160)
 
-local hexagon = Shape.RegPolygon(paper.center, 200, 6)
-      hexagon:setPen(black)
+for i = 1, 22 do
+  local pa1 = p1:clone()
+  local pa2 = p2:clone()
+  local pa3 = p3:clone()
+  local pa4 = p4:clone()
 
-square.closed = true
+  local distance = i * 2.65
 
-square:scale(3)
+  pa1:moveVector(baseAngle - 5, distance)
+  pa2:moveVector(baseAngle - 16, distance)
+  pa3:moveVector(baseAngle, distance)
+  pa4:moveVector(baseAngle, distance)
 
-local intersects = square:getIntersections(hexagon)
-
-local function dot(point)
-  local dot = Shape.RegPolygon(point, 10, 12)
-
-  dot:setPen(red)
-end
-
-for i, intersect in ipairs(intersects) do
-  dot(intersect)
+  local poly1 = Path:new(pa2, pa3, pa4)
+        poly1:setPen(blue)
+  
+  local poly2 = Path:new(pa4, pa1, pa2)
+        poly2:setPen(pink)
 end
 
 paper:saveTo('svg-output/testy.svg')
