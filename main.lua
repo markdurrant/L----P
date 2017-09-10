8538237fb7f42bfee8aed52af396566f9c4cde6d
Mark Durrant <mark.j.durrant@gmail.com>
2017-09-10 23:40:47 +0100

    more work on tree

--- main.lua
+++ main.lua
@@ -8,26 +8,88 @@ local Point = require("modules/point")
 local Shape = require("modules/shape")
 
 local paper = Paper:new({ width = 500, height = 500 })
-local black = Pen:new({ weight = 2, color = "#000" })
+local black = Pen:new({ weight = 1.5, color = "#000" })
+local pink = Pen:new({ weight = 1, color = "#f09" })
+local blue = Pen:new({ weight = 1, color = "#09f" })
 
 
 -- return a random point
 local function randomPoint()
-  return Point:new(
-    utl.random(40, paper.width - 40),
-    utl.random(40, paper.height - 40)
-  )
+  return Point:newVector(paper.center, utl.random(0, 360), 100)
 end
 
-local points = {}
+local function spacing(list, point)
+  local spacings = {}
 
-for i = 1, 100 do
-  Point:bestCandidate(points, randomPoint, 10)
+  for _, p in ipairs(list) do
+    table.insert(spacings, p:getDistanceTo(point))
+  end
+
+  table.sort(spacings, function(a, b) return a < b end)
+
+  return spacings[2] + spacings[3]
 end
 
-for _, p in ipairs(points) do
-  Shape.RegPolygon(p, 2, 6):setPen(black)
+local ringPoints = {}
+
+for i = 1, 60 do
+  Point:bestCandidate(ringPoints, randomPoint, 10)
 end
 
-paper:addPens(black)
+function drawRing()
+  local removals = {}
+  local additions = {}
+
+  for i, p in ipairs(ringPoints) do
+    if spacing(ringPoints, p) > 30 then
+      local p2 = Point:newVector(paper.center, p:getAngleTo(paper.center) - 2.5, - p:getDistanceTo(paper.center) - 18)
+      local p3 = Point:newVector(paper.center, p:getAngleTo(paper.center) + 2.5, - p:getDistanceTo(paper.center) - 18)
+
+      Path:new(
+        p2,
+        p:clone(),
+        p3
+      ):setPen(black)
+
+      table.insert(removals, i)
+      table.insert(additions, p2)
+      table.insert(additions, p3)
+    else
+      Path:new(
+        p:clone(),
+        p:clone():moveVector(p:getAngleTo(paper.center), - 18)
+      ):setPen(black)
+
+      p:moveVector(p:getAngleTo(paper.center), - 18)
+    end
+  end 
+
+  for i, r in ipairs(removals) do
+    -- print(r)
+    -- ringPoints[r] = nil
+    -- Shape.RegPolygon(ringPoints[r], 1, 6):setPen(blue)
+    print(#ringPoints, r)
+    table.remove(ringPoints, r - i + 1)
+  end
+
+  for _, a in ipairs(additions) do
+    table.insert(ringPoints, a)
+  end
+
+  return ringPoints
+end
+
+for _, p in ipairs(ringPoints) do
+  Shape.RegPolygon(p, 1, 6):setPen(pink)
+end
+
+drawRing()
+
+for _, p in ipairs(ringPoints) do
+  Shape.RegPolygon(p, 1, 6):setPen(pink)
+end
+
+drawRing()
+
+paper:addPens(black, pink, blue)
 paper:saveTo('svg-output/testy.svg')
