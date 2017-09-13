29542ca264bc4db7f943386373cc99da98fc1e6a
Mark Durrant <mark.j.durrant@gmail.com>
2017-09-13 01:02:44 +0100

    add waterfall drawing

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
-local start = os.clock()
-
 local utl = require("modules/utl")
 
 local Paper = require("modules/paper")
@@ -9,45 +7,9 @@ local Point = require("modules/point")
 
 local Shape = require("modules/shape")
 
-local paper = Paper:new({ width = 500, height = 500 })
-
-local black = Pen:new({ weight = 2, color = "#000" })
-local pink = Pen:new({ weight = 2, color = "#f09" })
-local blue = Pen:new({ weight = 2, color = "#09f" })
-
-local margin = 100
-
-local function randomPoint()
-  return Point:new(
-    math.random(margin, paper.width - margin),
-    math.random(margin, paper.height - margin)
-  )
-end
-
-local points = {}
-local paths = {}
-
-for i = 2, 100 do
-  Point:bestCandidate(points, randomPoint, 100)
-end
-
-local function line(point)
-  return Path:new(point, points[math.random(#points)])
-end
-
-for i, point in ipairs(points) do
-  Shape.RegPolygon(point, 2, 6):setPen(blue)
-
-  local line = line(point)
-
-  if math.fmod(i, 3) == 0 then
-    line:setPen(pink)
-    table.insert(paths, line)
-  end
-end
+local paper = Paper:new({ width = 210, height = 297 })
+local black = Pen:new({ weight = 1, color = "#000" })
 
 
-paper:addPens(blue, pink, black)
+paper:addPens(black)
 paper:saveTo('svg-output/testy.svg')
-
-print("completed in " .. start + os.clock() .. " seconds")
