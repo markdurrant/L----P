7264d047093b23fcbbc399a2fbe02639f372472f
Mark Durrant <mark.j.durrant@gmail.com>
2017-09-13 20:16:03 +0100

    first draft of lines from segment

--- main.lua
+++ main.lua
@@ -7,9 +7,74 @@ local Point = require("modules/point")
 
 local Shape = require("modules/shape")
 
-local paper = Paper:new({ width = 210, height = 297 })
-local black = Pen:new({ weight = 1, color = "#000" })
+local paper = Paper:new({ width = 400, height = 400 })
+
+local   black = Pen:new({ weight = 2, color = "#000" })
+
+local     red = Pen:new({ weight = 2, color = "#f00" })
+local   green = Pen:new({ weight = 2, color = "#0f0" })
+local    blue = Pen:new({ weight = 2, color = "#00f" })
+
+local    cyan = Pen:new({ weight = 2, color = "#0ff" })
+local fuchsia = Pen:new({ weight = 2, color = "#f0f" })
+local  yellow = Pen:new({ weight = 2, color = "#ff0" })
 
 
-paper:addPens(black)
+local dot = function(point, pen)
+  return Shape.RegPolygon(point, 3, 12):setPen(pen)
+end
+
+function Path:getLineFromSegment(distAlongPath, angle, distance)
+  local startPoint
+  local endPoint
+  local distAlongPath2 = distAlongPath
+
+  for i = 1, #self.points - 1 do
+    local segmentLength = self.points[i]:getDistanceTo(self.points[i + 1])
+
+    if distAlongPath2 > segmentLength then
+      distAlongPath2 = distAlongPath2 - segmentLength
+    elseif distAlongPath2 >= 0 then
+      distAlongPath2 = distAlongPath2 - segmentLength
+
+      startPoint = self.points[i]
+      endPoint = self.points[i + 1]
+    end
+  end
+
+  local initalPoint = self:getPointAtDistance(distAlongPath)
+
+  local point = initalPoint:clone()
+  
+  point:moveVector(startPoint:getAngleTo(endPoint) - angle, distance)
+
+  return initalPoint, point
+end
+
+local path1 = Path:new(
+  Point:new( 50,  50),
+  Point:new(200, 100),
+  Point:new(320, 220),
+  Point:new(350, 350)
+)
+
+
+for i = 0, path1:getLength() / 4 do
+  local point1, point2 = path1:getLineFromSegment(i * 4, 270, 4)
+
+  local path2 = Path:new(
+    point1,
+    point2
+  ):setPen(fuchsia)
+
+  local point1, point2 = path1:getLineFromSegment(i * 4, 90, 4)
+
+  local path2 = Path:new(
+    point1,
+    point2
+  ):setPen(fuchsia)
+end 
+
+
+paper:addPens(cyan, fuchsia, black)
 paper:saveTo('svg-output/testy.svg')
