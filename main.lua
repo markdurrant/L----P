b86d717dc8b5cc6d2f4f7f329e3b4a794d3e9be7
Mark Durrant <mark.j.durrant@gmail.com>
2018-03-05 19:05:22 +0000

    wiggle blobs

--- main.lua
+++ main.lua
@@ -1,14 +1,25 @@
 require('../L-P/L-P')
 
-math.randomseed(os.time())
+local devRandom = assert(io.open('/dev/random', 'rb')):read()				
+local devSeed = 0
+local random = 0
+        
+local dr = 1
+        		
+while dr < #devRandom and dr < 5 do
+  devSeed = devSeed .. devRandom:byte(dr)
+  dr = dr + 1
+end
+    		
+math.randomseed(tonumber(devSeed))
 
 local black = Pen({ weight = 0.5, color = '#000' })
 local cyan = Pen({ weight = 0.375, color = '#0ff' })
 local magenta = Pen({ weight = 0.375, color = '#f0f' })
 local yellow = Pen({ weight = 0.375, color = '#ff0' })
 
-local paper = Paper({ width = 100, height = 100 })
-      paper:add_pens({black, magenta, cyan, yellow})
+local paper = Paper({ width = 140, height = 100 })
+      paper:add_pens({black, cyan, magenta, yellow})
 
 
 local function wiggle_between(line_A, line_B, num_lines)
@@ -31,78 +42,53 @@ local function wiggle_between(line_A, line_B, num_lines)
   return Path(wiggle_points)
 end
 
-local function hex_wiggle(point, hex_size, num_lines, rotation)
-  hex_size = hex_size / 2
+local function get_blob(num_points, size, variance)
+  if num_points % 2 ~= 0 then num_points = num_points + 1 end
 
-  local line_A = Path({
-    point:clone():move_vector(360 / 6 * 0, hex_size),
-    point:clone():move_vector(360 / 6 * 1, hex_size),
-    point:clone():move_vector(360 / 6 * 2, hex_size),
-    point:clone():move_vector(360 / 6 * 3, hex_size)
-  })
-  
-  local line_B = Path({
-    point:clone():move_vector(360 / 6 * 0, hex_size),
-    point:clone():move_vector(360 / 6 * 5, hex_size),
-    point:clone():move_vector(360 / 6 * 4, hex_size),
-    point:clone():move_vector(360 / 6 * 3, hex_size)
-  })
+  local point_list = {}
 
-  local hex_wiggle = wiggle_between(line_A, line_B, num_lines)
-
-  return hex_wiggle:rotate(rotation)
-end
-
-local function get_random_point(x_min, x_max, y_min, y_max)
-  return Point(
-    math.random() * (x_max - x_min) + x_min,
-    math.random() * (y_max - y_min) + y_min
-  )
-end
-
-local function find_closest(point_list, point)
-  local distance = point_list[1]:distance_to(point)
-  local closest = point_list[1]
-  
-  for _, p in pairs(point_list) do
-    if p:distance_to(point) < distance then
-      distance = p:distance_to(point)
-      closest = p
-    end
-  end 
-
-  return closest
-end
-
-local function sample(point_list, num_candidates)
-  local best_candidate
-  local best_distance = 0
-
-  for c = 1, num_candidates do
-    local candidate = get_random_point(x_min, x_max, y_min, y_max)
-    local distance = find_closest(point_list, candidate):distance_to(candidate)
-
-    if distance > best_distance then
-      best_distance = distance
-      best_candidate = candidate
-    end
+  for i = 0, num_points - 1 do
+    table.insert(
+      point_list,
+      paper.center:clone():move_vector(360 / num_points * i, size / 2 + math.random() * variance  - variance / 2)
+    )
   end
 
-  return best_candidate
+  return Path(point_list):close()
 end
 
-x_min, x_max, y_min, y_max = 20, 80, 20, 80
+local function get_blob_lines(blob)
+  local line_A_points = {}
+  local line_B_points = {}
 
-local point_list = {get_random_point(x_min, x_max, y_min, y_max)}
+  for a = 1, #blob.points / 2 do
+    table.insert(line_A_points, blob.points[a])
+    table.insert(line_B_points, blob.points[#blob.points - a + 1])
+  end 
 
-for i = 1, 100 do
-  table.insert(point_list, sample(point_list, 10))
+  return {Path(line_A_points), Path(line_B_points)}
 end
 
-for _, p in pairs(point_list) do
-  hex_wiggle(p, 5.1, 11, 0):set_pen(cyan)
-  hex_wiggle(p, 5.1, 11, 120):set_pen(magenta)
-  hex_wiggle(p, 5.1, 11, 240):set_pen(yellow)
-end 
+local function get_blob_wiggle(num_points, size, variance, num_lines)
+  local blob = get_blob(num_points, size, variance)
+  local blob_lines = get_blob_lines(blob)
+  local blob_line_A = blob_lines[1]
+  local blob_line_B = blob_lines[2]
+  local blob_wiggle = wiggle_between(blob_line_A, blob_line_B, num_lines)
 
+  return blob_wiggle
+end
+
+local angle = math.random() * 360
+
+local blob_wiggle_A = get_blob_wiggle(12, 75, 15, 70):set_pen(yellow)
+local blob_wiggle_B = get_blob_wiggle(10, 50, 10, 50):set_pen(magenta)
+      blob_wiggle_B:rotate(120):move_vector(angle, math.random() * 10 + 5)
+local blob_wiggle_C = get_blob_wiggle(8, 25, 5, 25):set_pen(cyan)
+      blob_wiggle_C:rotate(240):move_vector(angle, math.random() * 20 + 10)
+
+
+cyan:save_gcode('cyan.nc')
+magenta:save_gcode('magenta.nc')
+yellow:save_gcode('yellow.nc')
 paper:save_svg('main.svg')
